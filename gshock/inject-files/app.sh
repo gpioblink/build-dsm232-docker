7ecb7b8b5a7d8e216a77350eb5e1cf70b412ea36
Kai Yamada <gpioblink@aol.com>
2020-07-15 12:33:33 +0900

    change directory stracture

--- gshock/inject-files/app.sh
+++ gshock/inject-files/app.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-echo "running gshock exploit was successful!" > hackinfo
-echo gc_warning $(cat hackinfo) > /cavium/remotectl_pipe
+env set bootm_image 'nand read ${RAM} 2400000 18e0000; bootm ${RAM}'
+env set bootcmd 'dcache on;run ledon; run show_logo; run bootm_image; boots'
+# env set bootargs 'console=/dev/fb0,115200'
+echo gc_warning "cameohax was successfully launched!\n\nWhen the message \"Firmware Upgrade Success\" appears,\nplease turn off the power and restart." > /cavium/remotectl_pipe
